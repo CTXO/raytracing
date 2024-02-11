53b5e569b1482442eb319ea645513d0c9adedbd1
Romero <rrcb@cin.ufpe.br>
2024-02-11 13:39:03 -0300

    feat: add rotation in Z-axis transformation

--- transformations.py
+++ transformations.py
@@ -1,10 +1,16 @@
 from abc import ABC, abstractmethod
+import math
 import numpy as np
 import numpy.typing as npt
 
 class Transformation(ABC):
     matrix: npt.ArrayLike
     
+    @staticmethod
+    def get_sin_cos(angle):
+        radians = math.radians(angle)
+        return math.sin(radians), math.cos(radians)
+
     def validate_matrix(self):
         if self.matrix.shape != (4, 4):
             raise ValueError(f"Matrix should be 4x4 but it is {self.matrix.shape[0]}x{self.matrix.shape[1]} instead. Matrix:\n{self.matrix}")
@@ -14,7 +20,7 @@ class Transformation(ABC):
             raise ValueError(f"Matrix should have {expected_last_row} as last row, but it has {last_row_of_matrix} instead. Matrix:\n{self.matrix}")
 
 
-class Transalation(Transformation):
+class Translation(Transformation):
     def __init__(self, x, y, z) -> None:
         self.matrix = np.array([
             [1, 0, 0, x],
@@ -23,3 +29,17 @@ class Transalation(Transformation):
             [0, 0, 0, 1]
         ])
         self.validate_matrix()
+
+
+
+class RotationZ(Transformation):
+    def __init__(self, angle) -> None:
+        sin, cos = self.get_sin_cos(angle)
+        self.matrix = np.array([
+            [cos, -sin, 0, 0],
+            [sin, cos, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+        self.validate_matrix()
+
