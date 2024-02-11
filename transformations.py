2af56a8a6f31160d60fd4f73b0ee2840a6840e92
Romero <rrcb@cin.ufpe.br>
2024-02-10 23:20:03 -0300

    feat: create translation transformation

--- transformations.py
+++ transformations.py
@@ -6,12 +6,20 @@ class Transformation(ABC):
     matrix: npt.ArrayLike
     
     def validate_matrix(self):
-        if not self.matrix.shape != (4, 4):
+        if self.matrix.shape != (4, 4):
             raise ValueError(f"Matrix should be 4x4 but it is {self.matrix.shape[0]}x{self.matrix.shape[1]} instead. Matrix:\n{self.matrix}")
         last_row_of_matrix = self.matrix[3]
         expected_last_row = np.array([0, 0, 0, 1])
         if not np.array_equal(last_row_of_matrix, expected_last_row):
             raise ValueError(f"Matrix should have {expected_last_row} as last row, but it has {last_row_of_matrix} instead. Matrix:\n{self.matrix}")
 
-    def __init__(self) -> None:
-        pass
+
+class Transalation(Transformation):
+    def __init__(self, x, y, z) -> None:
+        self.matrix = np.array([
+            [1, 0, 0, x],
+            [0, 1, 0, y],
+            [0, 0, 1, z],
+            [0, 0, 0, 1]
+        ])
+        self.validate_matrix()
